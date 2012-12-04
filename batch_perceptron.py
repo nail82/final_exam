6af0b80185649cb3c724c9adce5658c076a4a90d
Ted Satcher <tsatcher@dealnews.com>
2012-12-04 05:16:42 -0600

    Working the batch perceptron.

--- batch_perceptron.py
+++ batch_perceptron.py
@@ -28,4 +28,38 @@ def separate(data):
         and the second element is the weight vector
         itself.
     """
+    ldata = data.copy()
+    aug_data = np.matrix(map(augment_sample, ldata))
+    n = aug_data.shape[0]
+    wt_dimensions = aug_data.shape[1]
+
+    # Intial weigth vector
+    w = np.matrix(np.zeros(wt_dimensions)).T
+    total_count = 0
+    while True:
+        total_count += 1
+        w_delta = np.matrix(np.zeros(wt_dimensions)).T
+        # Check the classification of each sample
+        for i in range(0,len(aug_data)):
+            yk = aug_data[i].T
+            v = (w.T*yk)[0,0]
+            if v <= 0:
+                w_delta += yk
+        print w_delta
+        break
+
+
+def augment_sample(y):
+    """Augment a pattern vector and negate if the
+    pattern is a member of omega2"""
+    rtn = np.asarray(y).squeeze()
+    if rtn[0] > 1:
+        rtn[0] = 1
+        rtn = -rtn
+    return rtn
+
+def test():
     pass
+
+if __name__ == '__main__':
+    test()
