2e161ebfb793792a8fb6acbd52370a1ad3830311
Ted Satcher <tsatcher@dealnews.com>
2012-12-03 22:37:56 -0600

    Initial import of files and data for CS640
    final exam.

---
+++ batch_perceptron.py
@@ -0,0 +1,31 @@
+"""
+Ted Satcher
+CS 640
+Fall 2012
+
+Final Exam
+
+File: batch_perceptron.py
+
+This modulue implements the batch perceptron algorithm.
+"""
+from __future__ import print_function
+import numpy as np
+import itertools as it
+
+def separate(data):
+    """Use the batch perceptron algorithm to calculate
+    the decision function weight vector fo a two class problem.
+
+    Args:
+        data: A list of labeled sample vectors. The zeroth element
+            of each sample must identify the category this sample
+            belongs to.
+
+    Returns:
+        A tuple.  The first element is the number of
+        iterations to find the separating weight vector
+        and the second element is the weight vector
+        itself.
+    """
+    pass
