4ee4576418eb905d265df44ec3d3906da94b916b
Ted Satcher <tsatcher@dealnews.com>
2012-12-07 10:35:31 -0600

    Import of problem 4 code.

---
+++ parzen_window.py
@@ -0,0 +1,39 @@
+"""
+Ted Satcher
+CS 640
+Fall 2012
+
+Final Exam
+
+File: parzen_window.py
+
+This module defines the functions to create window
+functions for the Parzen window approach for estimating
+density functions.
+
+Each function created takes as arguments x and xi where
+x is the argument to the density function p(x) and xi
+is one sample from the set of sample patterns.
+"""
+
+import numpy as np
+def zero_one(h):
+    """This function creates the zero-one window function.
+    Equation 9, page 164, Duda et al."""
+    d = 1./h # d is the denominator of the window function.
+    def phi(x, xi):
+        v = np.abs(d * (x-xi))
+        if v <= 0.5:
+            return 1
+        else:
+            return 0
+    return phi
+
+def gauss(h):
+    """This function creates a Gaussian window function,
+    Equation 26, page 168, from Duda, et al."""
+    a = 1./np.sqrt(2*np.pi)
+    b = -.5 * (1./h)**2
+    def phi(x, xi):
+        return a * np.exp(b * (x-xi)**2)
+    return phi
---
+++ problem4.py
@@ -0,0 +1,20 @@
+"""
+Ted Satcher
+CS 640
+Fall 2012
+
+Final Exam
+
+File: problem4.py
+
+This executable is for Problem 4 for the final exam.
+It uses the Parzen window approach to calculate
+and estimated density function and also to plot
+the estimate.
+"""
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
