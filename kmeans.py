4e341b8295609f85f1fc54632c7cc592f88b8bc7
Ted Satcher <tsatcher@dealnews.com>
2012-12-07 20:26:43 -0600

    Import of files for 2 and 5.

---
+++ kmeans.py
@@ -0,0 +1,11 @@
+"""
+Ted Satcher
+CS 640
+Fall 2012
+
+Final Exam
+
+File: kmeans.py
+
+This module implements logic for k-means clustering.
+"""
---
+++ problem5.py
@@ -0,0 +1,11 @@
+"""
+Ted Satcher
+CS 640
+Fall 2012
+
+Final Exam
+
+File: problem5.py
+
+This is the executable for problem 5.
+"""
